a601012dc6c813d957ff350ddcaaa631563044f4
Vicky Gong <vickyg@mit.edu>
2014-10-17 14:37:06 -0400

    Removing callback functions as param

--- public/javascripts/AjaxController.js
+++ public/javascripts/AjaxController.js
@@ -1,36 +1,27 @@
 var AjaxController = function() {
-  this.get = function(url, doneCallback, failCallback) {
-    return ajaxReq("GET", url, null, doneCallback, failCallback);
+  this.get = function(url) {
+    return ajaxReq("GET", url, null);
   }
 
-  this.post = function(url, data, doneCallback, failCallback) {
-    return ajaxReq("POST", url, data, doneCallback, failCallback);
+  this.post = function(url, data) {
+    return ajaxReq("POST", url, data);
   }
 
-  this.put = function(url, data, doneCallback, failCallback) {
-    return ajaxReq("PUT", url, data, doneCallback, failCallback);
+  this.put = function(url, data) {
+    return ajaxReq("PUT", url, data);
   }
 
-  this.del = function(url, data, doneCallback, failCallback) {
-    return ajaxReq("DELETE", url, data, doneCallback, failCallback);
+  this.del = function(url, data) {
+    return ajaxReq("DELETE", url, data);
   }
 
-  var ajaxReq = function(type, url, data, doneCallback, failCallback) {
+  var ajaxReq = function(type, url, data) {
     return $.ajax({
       datatype: "json", 
       type: type, 
       url: url, 
       data: data
-    }).done(function(res) {
-      doneCallback(res);
-    }).fail(function(e) {
-      if (failCallback !== undefined && typeof failCallback == "function") {
-        failCallback(e);
-      } else {
-        console.log("ERROR: " + type + " request to " + url + " failed with the following error: \n" + e.responseText);
-      }
     });
-  }
 }
 
 var ajaxController = new AjaxController();
