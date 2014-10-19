fab4553b8492d73b1617c3817f3cf1f90be9dc41
Calvin Li <calvinli@mit.edu>
2014-10-19 13:02:13 -0400

    added contribution model, created RESTful routes for contribution model

--- data/models/contribution.js
+++ data/models/contribution.js
@@ -2,3 +2,5 @@ var mongoose = require('mongoose');
 var ContributionSchema = require('../schemas/contribution');
 
 var Contribution = mongoose.model('Contribution', ContributionSchema);
+
+var module.exports = Contribution;
