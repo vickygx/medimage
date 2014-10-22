26e25e85816b5e4484bbf4a18d18738c65af48e9
Calvin Li <calvinli@mit.edu>
2014-10-21 20:25:23 -0400

    changed db name in production

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
   dbURL = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
           process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
           process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-          process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + dbName;
+          process.env.OPENSHIFT_MONGODB_DB_PORT + '/medimage';
 }
 var db = require('mongoose').connect(dbURL, function() {
   console.log("Successfully connected to MongoDB at: \n", dbURL);
